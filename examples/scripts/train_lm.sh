a2169d7abd49aa00e8aa92179884c056d572be1b
Julius Steuer <jsteuer@lsv.uni-saarland.de>
2022-12-14 09:22:55 +0100

    save evaluation results in smaller files
    - writing all surprisal values to a single file proved impractical, now results are saved per batch
    - changed sample lm train script to use an lstm

--- examples/scripts/train_lm.sh
+++ examples/scripts/train_lm.sh
@@ -4,8 +4,8 @@ source /languagemodels/examples/scripts/setup.sh
 
 
 DATA_DIR="/datasets/wikitext-103-raw"
-CONFIG_PATH="/languagemodels/examples/configs/gru/basic_gru.json"
-OUTPUT_DIR="/logfiles"
+CONFIG_PATH="/languagemodels/examples/configs/lstm/basic_lstm.json"
+OUTPUT_DIR="/languagemodels/logfiles"
 
 train_lm \
     --wandb_project_name languagemodels \
