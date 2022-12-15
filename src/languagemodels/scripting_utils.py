7907cb2f24772d46f5c498e1f69dd20c228c06ea
Julius Steuer <jsteuer@lsv.uni-saarland.de>
2022-12-15 10:29:01 +0100

    count tokens the correct way when training on masked data
    - still have to decide how it is determined whether a model counts a masked or not

--- src/languagemodels/scripting_utils.py
+++ src/languagemodels/scripting_utils.py
@@ -8,3 +8,11 @@ def repackage_hidden(hidden_state):
         return hidden_state.detach()
     else:
         return tuple(repackage_hidden(t) for t in hidden_state)
+
+
+def get_num_occurrences_in_tensor(value: int, t: torch.Tensor) -> int:
+    word_ids, word_counts = torch.unique(t, return_counts=True)
+    value_count = (word_counts == value).nonzero(as_tuple=True)
+    if value_count.numel() == 0:
+        return 0
+    return value_count.item()
